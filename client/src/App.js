19e8ec1424581a2ecf8322352225bfc63dcfd94b
Cem <cmbicakci@gmail.com>
2022-09-30 21:39:24 +0300

    Outlet

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,65 @@
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Single from './pages/Single'
+import Write from './pages/Write'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/single",
+        element: <Single />
+      },
+      {
+        path: "write",
+        element: <Write />
+      }
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
