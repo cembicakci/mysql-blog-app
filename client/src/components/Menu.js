135dea1bd493445c33f39424ccd1c96c301d7fb9
Cem <cmbicakci@gmail.com>
2022-10-01 21:28:59 +0300

    menu page

---
+++ client/src/components/Menu.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Menu() {
+
+    const posts = [
+        {
+            id: 1,
+            title: "My Gallery 1",
+            desc: "Lorem discContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage",
+            img: 'https://images.pexels.com/photos/13632955/pexels-photo-13632955.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        },
+        {
+            id: 2,
+            title: "My Gallery 2",
+            desc: "Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.. comes from a line in section 1.10.32",
+            img: 'https://images.pexels.com/photos/12737062/pexels-photo-12737062.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        }
+    ]
+
+    return (
+        <div className='menu'>
+            <h1>Other posts you may like</h1>
+
+            {posts.map(post => (
+                <div className='post'>
+                    <img src={post.img} />
+                    <h2>{post.title}</h2>
+                    <button>Read more</button>
+
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Menu
--- client/src/pages/Single.js
+++ client/src/pages/Single.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Edit from '../img/edit.png'
+import Delete from '../img/delete.png'
+import Menu from '../components/Menu'
 
 function Single() {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className='content'>
+        <img src="https://images.pexels.com/photos/13298639/pexels-photo-13298639.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
+
+        <div className='user'>
+          <img src='https://images.pexels.com/photos/12800709/pexels-photo-12800709.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
+
+          <div className='info'>
+            <span>Cem</span>
+            <p>Posted 2 days ago</p>
+          </div>
+
+          <div className='edit'>
+            <Link to={`/write?edit=1`}>
+              <img src={Edit} alt='edit'></img>
+            </Link>
+            <img src={Delete} alt='delete'></img>
+          </div>
+        </div>
+        <h1>The standard Lorem Ipsum passage, used since the 1500s</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <Menu />
+    </div>
   )
 }
 
