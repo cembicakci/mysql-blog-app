945906250755cd935be278eb0b5291286063f77f
Cem <cmbicakci@gmail.com>
2022-10-05 21:08:53 +0300

    added posts routes

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,19 @@
+export const getPosts = (req, res) => {
+    res.json('from controllers')
+} 
+
+export const getPost = (req, res) => {
+    res.json('from controllers')
+} 
+
 export const addPost = (req, res) => {
+    res.json('from controllers')
+} 
+
+export const deletePost = (req, res) => {
+    res.json('from controllers')
+} 
+
+export const updatePost = (req, res) => {
     res.json('from controllers')
 } 
